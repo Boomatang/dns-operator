cbb8526f3b33446e11f9afc8876d8388cd0bc95b
Jim Fitzpatrick <jfitzpat@redhat.com>
2026-04-07 17:41:27 +0100

    [release] Prepare release v0.6.0

---
+++ make/release.mk
@@ -0,0 +1,6 @@
+#Release default values
+IMG=quay.io/kuadrant/dns-operator:v0.6.0
+BUNDLE_IMG=quay.io/kuadrant/dns-operator-bundle:v0.6.0
+CATALOG_IMG=quay.io/kuadrant/dns-operator-catalog:v0.6.0
+CHANNELS=stable
+BUNDLE_CHANNELS=--channels=stable
